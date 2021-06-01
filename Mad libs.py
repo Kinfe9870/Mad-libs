44978a0a4d4f69f5ea23de196bd1fc6d17f0380e
Kinfe9870 <80673190+Kinfe9870@users.noreply.github.com>
2021-06-01 13:59:08 +0100

    Add files via upload

---
+++ Mad libs.py
@@ -0,0 +1,7 @@
+adjective = input("Enter an adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter the room number: ")
+
+print("It was a " + adjective + " windy day in November." \
+                                "I was in room " + room + " looking out " \
+                                                          "of my window watching the " + bird + " sing")
